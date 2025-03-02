f2ecb4ea10f03b38d1f405ebace08429c76f301f
kanda <kandavelece906@gmail.com>
2025-03-02 17:27:37 +0530

    added an output

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("First step");
+        System.out.println("Added to git");
     }
 }
