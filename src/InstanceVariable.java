9c96ff4e14de5e7e1c5ef6a9d3f8422fc9ee2a49
kanda <kandavelece906@gmail.com>
2025-03-04 00:53:31 +0530

    Added instance and static variable

---
+++ src/InstanceVariable.java
@@ -0,0 +1,9 @@
+class InstanceVariable {
+    public int a= 20;
+    public String name;
+
+    public InstanceVariable(){
+        name = "Java";
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+class Student{
+    static int RollNo;
+}
+
+
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,5 +14,16 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("First step");
         System.out.println("Added to git");
+
+        Student s1 = new Student();
+        Student s2 =  new Student();
+        s1.RollNo =  10;
+        Student.RollNo = 20;
+
+        System.out.println("The roll no "+s1.RollNo);
+
+        InstanceVariable data = new InstanceVariable();
+
+        System.out.println(data.a);
     }
 }
