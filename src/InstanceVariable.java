967fb025086c8cc7236b408b1b06f7ae3366bcdc
kanda <kandavelece906@gmail.com>
2025-03-04 17:47:36 +0530

    Adding to repository

--- src/InstanceVariable.java
+++ src/InstanceVariable.java
@@ -1,9 +1,23 @@
-class InstanceVariable {
-    public int a= 20;
+public class InstanceVariable {
+    public int a = 20;
     public String name;
 
-    public InstanceVariable(){
+    public InstanceVariable() {
         name = "Java";
     }
+
+
+//    public static void main(String[] args) {
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+//        System.out.println("First step");
+//        System.out.println("Added to git");
+//
+//
+//        InstanceVariable data = new InstanceVariable();
+//
+//        System.out.println(data.name);
+//    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -20,10 +20,18 @@ public class Main {
         s1.RollNo =  10;
         Student.RollNo = 20;
 
+        int a = 20;
+
         System.out.println("The roll no "+s1.RollNo);
+
+        Student.RollNo++;
+        System.out.println(++Student.RollNo);
+        System.out.println(Student.RollNo + ++Student.RollNo);
+
+        System.out.println(a);
 
         InstanceVariable data = new InstanceVariable();
 
-        System.out.println(data.a);
+        System.out.println(data.a +" " +data.name);
     }
 }
